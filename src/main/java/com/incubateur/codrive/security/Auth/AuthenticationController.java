d654fe449c1646e89ef31176e173536f0f504cf3
tomab23 <thomas.bartier59@gmail.com>
2023-05-08 20:19:22 +0200

    CORS

--- src/main/java/com/incubateur/codrive/security/Auth/AuthenticationController.java
+++ src/main/java/com/incubateur/codrive/security/Auth/AuthenticationController.java
@@ -2,14 +2,12 @@ package com.incubateur.codrive.security.Auth;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173", maxAge = 3600)
 public class AuthenticationController {
     private final AuthenticationService service;
     @PostMapping("/register")
